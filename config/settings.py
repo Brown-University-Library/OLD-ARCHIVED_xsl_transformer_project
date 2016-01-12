7be81ffbd87db2e7db9d74faf56b8a1f6b461bfc
birkin <birkin.diana@gmail.com>
2016-01-12 12:00:49 -0500

    updates log-format.

--- config/settings.py
+++ config/settings.py
@@ -109,8 +109,8 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'standard': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        'format': "[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
         },
     },
     'handlers': {
