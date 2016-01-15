68e87adc8cc0398e64d8723082160d27a9a81df9
birkin <birkin.diana@gmail.com>
2016-01-15 07:14:56 -0500

    removes old settings.

--- transformer_app/settings_app.py
+++ transformer_app/settings_app.py
@@ -4,10 +4,20 @@ from __future__ import unicode_literals
 import json, os
 
 
-LEGIT_IPS = json.loads( os.environ['XSL__LEGIT_IPS_JSON'] )
-LEGIT_GET_KEYS = json.loads( os.environ['XSL__LEGIT_GET_KEYS_JSON'] )
-LEGIT_POST_KEYS = json.loads( os.environ['XSL__LEGIT_POST_KEYS_JSON'] )
-
 LEGIT_IPS_KEYS = json.loads( os.environ['XSL_LEGIT_IPS_KEYS_JSON'] )
+
+## example XSL_LEGIT_IPS_KEYS_JSON setting (get a random key at `http://127.0.0.1/xsl_transformer/keymaker/`) ##
+# export XSL_LEGIT_IPS_KEYS_JSON='
+#     {
+#       "label_a": {
+#         "ip_auth_key": "some_key_a",
+#         "legit_ip": "some_ip_a"
+#       },
+#       "label_b": {
+#         "ip_auth_key": "some_key_b",
+#         "legit_ip": "some_ip_b"
+#       }
+#     }
+#     '
 
 SAXON_CLASSPATH = os.environ['XSL__SAXON_CLASSPATH']
