5e00054ffa2df12d3b0b9247b3ef33d9e218e091
birkin <birkin.diana@gmail.com>
2016-01-20 10:23:28 -0500

    updates docs.

--- transformer_app/settings_app.py
+++ transformer_app/settings_app.py
@@ -6,19 +6,22 @@ import json, os
 
 LEGIT_IPS_KEYS = json.loads( os.environ['XSL__LEGIT_IPS_KEYS_JSON'] )
 
-## example XSL__LEGIT_IPS_KEYS_JSON setting (get a random key at `http://127.0.0.1/xsl_transformer/keymaker/`) ##
-# export XSL__LEGIT_IPS_KEYS_JSON='
-#     {
-#       "label_a": {
-#         "ip_auth_key": "some_key_a",
-#         "legit_ip": "some_ip_a"
-#       },
-#       "label_b": {
-#         "ip_auth_key": "some_key_b",
-#         "legit_ip": "some_ip_b"
-#       }
-#     }
-#     '
+"""
+example XSL__LEGIT_IPS_KEYS_JSON setting (see your ip and get a random key at `http://host/xsl_transformer/keymaker/`):
+
+export XSL__LEGIT_IPS_KEYS_JSON='
+    {
+      "label_a": {
+        "ip_auth_key": "some_key_a",
+        "legit_ip": "some_ip_a"
+      },
+      "label_b": {
+        "ip_auth_key": "some_key_b",
+        "legit_ip": "some_ip_b"
+      }
+    }
+    '
+"""
 
 WHITELISTED_HOSTS = json.loads( os.environ['XSL__WHITELISTED_HOSTS_JSON'] )
 
