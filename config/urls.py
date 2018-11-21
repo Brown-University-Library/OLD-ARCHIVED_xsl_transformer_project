952fa3687d2b3d2a621a87573d049c740a913a36
birkin <birkin.diana@gmail.com>
2018-11-21 10:32:19 -0500

    updates urls to modern django syntax.

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.views.generic import RedirectView
+from transformer_app import views
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
 
-    url( r'^admin/', include(admin.site.urls) ),
+urlpatterns = [
 
-    url( r'^', include('transformer_app.urls_app') ),
+    url( r'^admin/', admin.site.urls ),
 
-)
+    url( r'^v1/$', views.run_transform_v1, name='transformer_v1_url' ),
+    url( r'^v1/shib/$', views.run_transform_v1, name='transformer_v1_url' ),
+
+    url( r'^keymaker/$', views.keymaker, name='keymaker_url' ),
+
+    url( r'^info/$', views.info, name='info_url' ),
+
+    url( r'^$', RedirectView.as_view(pattern_name='info_url') ),
+
+    ]
+
+
+# urlpatterns = patterns('',
+
+#     url( r'^admin/', include(admin.site.urls) ),
+
+#     url( r'^', include('transformer_app.urls_app') ),
+
+# )
